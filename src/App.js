2af579069f96901569965fa0926de1376192f825
Isaiah Bae <isaiahbae@Isaiahs-MacBook-Air.local>
2025-03-31 20:56:21 -0500

    new css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> Hi my name is Isaiah</p>
+        
+          
+        <p className="Header"> To-Do List: </p>
       </header>
     </div>
   );
