111810ef8dac2f775affd627e1ec47264dd3b8e8
isaihb <isaiahtechboy@gmail.com>
2025-04-12 15:10:29 -0500

    added a remove button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       setInputValue("");
     }
 
+    
   }
 
 
@@ -29,17 +30,40 @@ function App() {
       
       <p className="Header"> To-Do List: </p>
       
-      <label className = "addItem">Add Your Item Here: </label>
-      <input type = "text" className = "textBox" onKeyDown = {handleKeyPress} onChange={(e) => setInputValue(e.target.value)}/>
+      <label className="addItem">Add Your Item Here: </label>
+      <input 
+        type="text" 
+        className="textBox" 
+        onKeyDown={handleKeyPress} 
+        onChange={(e) => setInputValue(e.target.value)} 
+        value={inputValue}
+      />
       <ul className="list">
         {items.map((item, index) => (
-          <li key={index} className="listItem">{item}</li>
+          <li key={index} className="listItem">
+            {item}
+            <button className = "specialButton" onClick={() => handleRemoveItem(index)}>Remove</button>
+          </li>
         ))}
       </ul>
       </header>
     </div>
-    
   );
+
+  function handleRemoveItem(index) {
+    const updatedItems = [];
+
+  // Go through each item in the original items list
+  for (let i = 0; i < items.length; i++) {
+    // If the current index is not the one we want to remove, keep the item
+    if (i !== index) {
+      updatedItems.push(items[i]);
+    }
+  }
+
+  // Update the state with the new list that doesn't include the removed item
+  setItems(updatedItems);
+  }
 }
 
 export default App;
